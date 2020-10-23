f26ab19d258fc1d9832f140ae50d7463ad5b4af9
andy <andy@delusion.org>
2020-10-23 07:20:53 -0700

    Add project files.

---
+++ engineofpain/Main.cpp
@@ -0,0 +1,16 @@
+// ENGINE OF PAIN aka KatEngine
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+#include <iostream>
+
+int main() {
+	
+	std::cout << "Fuck you haydon\n";
+	std::cout << "now this is the story all about how\nmy life got flipped turned upside down.\n";
+	std::cerr << "ENGINE PAIN\n";
+
+	return 0;
+}
